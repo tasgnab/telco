d434f6ffa4d35b781d27f88b31b4613c19e88fce
Thopo Martha Akbar <thopo.martha@gmail.com>
2017-10-19 22:25:08 +0700

    add search PO

--- application/controllers/Po.php
+++ application/controllers/Po.php
@@ -57,31 +57,39 @@ class Po extends MY_Controller {
 
 	public function search_po(){
 		if($this->input->post()){
-			$data['po_no'] = $this->input->post('po_no');
-			$tmp = $this->input->post('po_date');
-			$po_date = explode('-',$tmp);
-			$po_date_min = trim($po_date[0]);
-			$po_date_max = trim($po_date[1]);
-			$data['po_date'] = $tmp->format('Y-m-d');
-			$data['project_name'] = $this->input->post('project_name');
-			$data['customer'] = $this->input->post('customer');
-			$data['area'] = $this->input->post('area');
-			$data['site_code'] = $this->input->post('site_code');
-			$data['site_name'] = $this->input->post('site_name');
-			$data['item'] = $this->input->post('item');
-			$data['unit_price'] = (float) str_replace(",","",$this->input->post('unit_price'));
-			$data['quantity'] = (int) $this->input->post('quantity');
-			$tmp = new DateTime($this->input->post('start_date'));
-			$data['start_date'] = $tmp->format('Y-m-d');
-			$tmp = new DateTime($this->input->post('end_date'));
-			$data['end_date'] = $tmp->format('Y-m-d');
-			$data['work_order'] = $this->input->post('work_order');
-			$data['po_value'] = (float) $data['unit_price'] * $data['quantity'];
+			if ($this->input->post('po_no')){
+				$data['po_no'] = $this->input->post('po_no');
+			}
+			if ($this->input->post('po_date')){
+				$tmp = $this->input->post('po_date');
+				$po_date = explode('-',$tmp);
+				$po_date_min = new DateTime(trim($po_date[0]));
+				$data['po_date_min'] = $po_date_min->format('Y-m-d');
+				$po_date_max = new DateTime(trim($po_date[1]));
+				$data['po_date_max'] = $po_date_max->format('Y-m-d');		
+			}
+			if ($this->input->post('project_name')){
+				$data['project_name'] = $this->input->post('project_name');
+			}
 			
+			if($this->input->post('customer')){
+				$data['customer'] = $this->input->post('customer');
+			}
+			if($this->input->post('area')){
+				$data['area'] = $this->input->post('area');
+			}
+			if($this->input->post('site_code')){
+				$data['site_code'] = $this->input->post('site_code');
+			}
+			if($this->input->post('site_name')){
+				$data['site_name'] = $this->input->post('site_name');
+			}
 			$this->load->model('M_Po');
-			$this->M_Po->insertPO($data);
-			$this->session->set_flashdata('message','New Purchase Order Inserted');
+			log_message('debug', print_r($data, TRUE));
+			$data['polist'] = $this->M_Po->searchPO($data);
+			$this->load->view('dashboard/po_list',$data);
+		} else {
+			$this->load->view('dashboard/po_search');
 		}
-		$this->load->view('dashboard/po_search');
 	}
 }
--- application/models/M_Po.php
+++ application/models/M_Po.php
@@ -17,4 +17,33 @@ class M_Po extends MY_Model {
 		$this->db->order_by('id', 'asc');
 		return $this->db->get()->result();
 	}
+
+	function searchPO($data){
+		$this->db->select('*');
+		$this->db->from('purchase_order');
+		if (array_key_exists('po_no', $data)){
+			$this->db->where('po_no', $data['po_no']);
+		}
+		if (array_key_exists('po_date_min', $data)){
+			$this->db->where('po_date >=', $data['po_date_min']);
+			$this->db->where('po_date <=', $data['po_date_max']);
+		}
+		if (array_key_exists('project_name', $data)){
+			$this->db->like('project_name', $data['project_name']);
+		}
+		if (array_key_exists('customer', $data)){
+			$this->db->like('customer', $data['customer']);
+		}
+		if (array_key_exists('area', $data)){
+			$this->db->like('area', $data['area']);
+		}
+		if (array_key_exists('site_code', $data)){
+			$this->db->where('site_code', $data['site_code']);
+		}
+		if (array_key_exists('site_name', $data)){
+			$this->db->like('site_name', $data['site_name']);
+		}
+		$this->db->order_by('id', 'asc');
+		return $this->db->get()->result();
+	}
 }
